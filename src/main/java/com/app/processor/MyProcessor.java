d32c216856ebbf542db8e5f586713fe1210220aa
new <new@DESKTOP-TCT062V>
2019-07-31 09:47:34 +0530

    comment

---
+++ src/main/java/com/app/processor/MyProcessor.java
@@ -0,0 +1,18 @@
+package com.app.processor;
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.app.model.Product;
+
+public class MyProcessor implements ItemProcessor<Product, Product>{
+
+	public Product process(Product p) throws Exception {
+		double cost=p.getCost();
+		p.setDisc(cost*3/100.0);
+		p.setGst(cost*12/100.0);
+		return p;
+	}
+
+}
+
